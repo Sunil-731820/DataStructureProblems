e1272c5254d388b91daedf656f626eb456a2ec98
Sunil-731820 <sunilkumargupta1170@gmail.com>
2022-05-19 09:26:33 +0530

    updated code

---
+++ src/main/java/com/java/ds/Codechef/chess.java
@@ -0,0 +1,13 @@
+package com.java.ds.Codechef;
+
+import java.util.Scanner;
+
+public class chess {    
+    public static void main(String[] args) {
+        int t= 4;
+        while(t--!=0){
+           System.out.println("hello");
+        }
+    }
+    
+}
---
+++ src/main/java/com/java/ds/Codechef/submission.java
@@ -0,0 +1,17 @@
+package com.java.ds.Codechef;
+
+import java.util.Scanner;
+
+public class submission {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+            int n = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            a = n-a;
+            b = a-b;
+            System.out.println(a +" "+b);
+       
+    }
+}
