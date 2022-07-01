a9bcd6248292c1fdd2a9598a8f6635ad249135fb
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2022-07-01 12:21:47 +0530

    updated

--- src/main/java/com/java/ds/Graph/BreadthFirstSearchAlgorithms.java
+++ src/main/java/com/java/ds/Graph/BreadthFirstSearchAlgorithms.java
@@ -1,5 +1,32 @@
 package com.java.ds.Graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BreadthFirstSearchAlgorithms {
+	private int V;
+	private LinkedList<Integer> adj[];
+	private Queue<Integer> queue;
+	
+	BreadthFirstSearchAlgorithms(int v){
+		V = v;
+		adj = new LinkedList[v];
+		for(int i=0;i<v;i++) {
+			adj[i] = new LinkedList<>();
+		}
+		queue = new LinkedList<Integer>();
+	}
+	
+	void addEdge(int v , int w) {
+		adj[v].add(w);
+	}
+	
+	void BFS(int n) {
+		
+	}
+	public static void main(String[] args) {
+		System.out.println("hello");
+		
+	}
 
 }
