f2a78a05e5e908e25427026d889c41f0257a8949
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-05-23 00:47:00 +0530

    "coded"

--- src/main/java/com/CollectionsInJava/ContentRelatedToListInJava/SecondLinkedListExample/FirstEXampleOfLinkedList.java
+++ src/main/java/com/CollectionsInJava/ContentRelatedToListInJava/SecondLinkedListExample/FirstEXampleOfLinkedList.java
@@ -7,7 +7,7 @@ import javax.sound.sampled.Line;
 
 public class FirstEXampleOfLinkedList {
     public static void main(String[] args) {
-        System.out.println("This is The Example Of THe Linkedlist ");
+        System.out.println("This is The Example Of THe Linkedlist  ");
         LinkedList<String> linkedList = new LinkedList<>();
         linkedList.add("Ravi");
         linkedList.add("vijay");
@@ -17,7 +17,7 @@ public class FirstEXampleOfLinkedList {
         System.out.println(linkedList);
         System.out.println("After The Use of The Iterator ");
         Iterator itr = linkedList.iterator();
-        while(itr.hasNext()){
+        while (itr.hasNext()) {
             System.out.println(itr.next());
         }
     }
