81793088c7f8594436dcef5510054a81daaca5af
Sunil Gupta <sunilg@infinite.com>
2023-06-22 12:49:45 +0530

    merging the Two Array And SOrting is Completed

--- src/main/java/com/java/ds/AllCodingQuestionsAskedInCompanies/Arrays/MergeTwoSortedArrayInSingleArray.java
+++ src/main/java/com/java/ds/AllCodingQuestionsAskedInCompanies/Arrays/MergeTwoSortedArrayInSingleArray.java
@@ -4,6 +4,33 @@ import java.util.Scanner;
 
 public class MergeTwoSortedArrayInSingleArray {
 	
+	static void sortTheGivenMergeArray(int[] arr , int size) {
+		for(int i=0;i<size;i++) {
+			for(int j=i;j<size;j++) {
+				if(arr[i]>arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		System.out.println("The Array is AFter Sorting the Merging Array is ");
+		for(int i=0;i<size;i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+	
+	static void addTwoArrayBeforeMerge(int[] arr1 , int[] arr2 , int size1 , int size2) {
+		int totalSize = size1+ size2;
+		for(int i=0;i<totalSize;i++) {
+			arr1[size1+1] = arr2[i];  
+		}
+		System.out.println("The Array is After Adding into one Is ");
+		for(int i=0;i<totalSize;i++) {
+			System.out.print(arr1[i] + " ");
+		}
+	}
+	
 	static void mergeTwoSortedArrayInToSingleArray(int[] arr1 , int[] arr2 , int size1,int size2) {
 		int totalSize = size1 + size2;
 		System.out.println("The Total Size of The Array is : "+ totalSize);
@@ -12,7 +39,7 @@ public class MergeTwoSortedArrayInSingleArray {
 			thirdArray[i] = arr1[i];
 		}
 		for(int i=0;i<size2;i++) {
-			thirdArray[i] = arr2[i];
+			thirdArray[i+size1] = arr2[i];
 		}
 		
 		
@@ -20,6 +47,8 @@ public class MergeTwoSortedArrayInSingleArray {
 		for(int i=0;i<totalSize;i++) {
 			System.out.print(thirdArray[i]+ " ");
 		}
+		System.out.println("calling The method called Sort is : ");
+		sortTheGivenMergeArray(thirdArray, totalSize);
 	}
 	public static void main(String[] args) {
 		System.out.println("This is The Example Of The Merging The Two Sorted Array");
@@ -38,6 +67,7 @@ public class MergeTwoSortedArrayInSingleArray {
 			System.out.println("Enter The Element In The Second Array ");
 			secondArray[j] = sc.nextInt();
 		}
+//		addTwoArrayBeforeMerge(firstArray, secondArray, size1, size2);
 		mergeTwoSortedArrayInToSingleArray(firstArray, secondArray, size1, size2);
 	}
 
