8b10d79e0b93562991fd1879b0272f33f76c3772
Sunil Gupta <sunilg@infinite.com>
2023-06-26 10:31:26 +0530

    Continue With Given LinkedList Problem

---
+++ src/main/java/com/java/ds/AllCodingQuestionsAskedInCompanies/LinkedList/FirstExampleOfLinkedList.java
@@ -0,0 +1,8 @@
+package com.java.ds.AllCodingQuestionsAskedInCompanies.LinkedList;
+
+public class FirstExampleOfLinkedList {
+	public static void main(String[] args) {
+		System.out.println("This is The First Example Of The LinkedList ");
+	}
+
+}
