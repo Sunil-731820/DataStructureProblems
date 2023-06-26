31a17033bcb306d816e59337ce40eb05bc797e05
Sunil Gupta <sunilg@infinite.com>
2023-06-26 18:08:35 +0530

    Append operation is Completed In LinkedList

--- src/main/java/com/java/ds/AllCodingQuestionsAskedInCompanies/LinkedList/LinkedListInsertionWithDifferentPoints.java
+++ src/main/java/com/java/ds/AllCodingQuestionsAskedInCompanies/LinkedList/LinkedListInsertionWithDifferentPoints.java
@@ -1,5 +1,7 @@
 package com.java.ds.AllCodingQuestionsAskedInCompanies.LinkedList;
 
+import java.util.Scanner;
+
 class Node{
 	
 	int data;
@@ -27,13 +29,30 @@ class LinkedList{
 		head = new_node;
 	}
 	
+	void append(int new_data) {
+		Node new_node = new Node(new_data);
+		if(head==null) {
+			head = new_node;
+			return;
+		}
+		else {
+			Node last = head;
+			while(last.next!=null) {
+				last = last.next ;
+			}
+			last.next = new_node;
+		}
+		
+	}
+	
+//	Displaying The Linkedlist 
 	void printListOfTheLinkedList() {
 		Node node = head;
 		while(node!=null) {
 			System.out.print(node.data + " ->");
 			node = node.next;
 		}
-		System.out.print("NULL");
+		
 	}
 }
 
@@ -41,9 +60,25 @@ public class LinkedListInsertionWithDifferentPoints {
 	public static void main(String[] args) {
 		System.out.println("This is The Example Of The LinkedListInsertionWithDifferentPoints ");
 		LinkedList list = new LinkedList();
-		list.push(12);
-		list.push(23);
-		list.push(34);
+//		list.push(12);
+//		list.push(23);
+//		list.push(34);
+		list.printListOfTheLinkedList();
+		Scanner sc = new Scanner(System.in);
+//		Taking The user Input 
+//		System.out.println("Example Of Taking This Input from The User ");
+//		System.out.println("ENter The Size of The linkedList ");
+//		int size = sc.nextInt();
+//		for(int i=0;i<size;i++) {
+//			System.out.println("Enter The New Node In The Given linked List");
+//			int number = sc.nextInt();
+//			list.push(number);
+//		}
+//		list.printListOfTheLinkedList();
+		
+//		Appending the Linked List node At The End of The Given LinkedList 
+		list.append(21);
+		list.append(23);
 		list.printListOfTheLinkedList();
 	}
 
