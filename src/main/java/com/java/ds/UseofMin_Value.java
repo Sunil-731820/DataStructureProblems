fb3a29e3a9512314ed3b044687f70513a9222a89
Sunil Gupta <sunilg@infinite.com>
2023-07-27 10:37:32 +0530

    CHanges Are Done in Modifier

--- src/main/java/com/java/ds/UseofMin_Value.java
+++ src/main/java/com/java/ds/UseofMin_Value.java
@@ -1,9 +1,34 @@
 package com.java.ds;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class UseofMin_Value {
 	public static void main(String[] args) {
-		System.out.println(Integer.MIN_VALUE);
-		System.out.println(Integer.MAX_VALUE);
+		String str = "";
+		System.out.println(str);
+		str= "sunil";
+		System.out.println(str);
+//		List<Integer> list = new ArrayList<Integer>();
+////		list.add(100);
+//		System.out.println(list);
+//		Iterator itr = list.iterator();
+//		System.out.println("After The Use Of The Iterator ");
+//		try {
+//			System.out.println("Inside The Try ");
+//			while(itr.hasNext()) {
+//				System.out.println("Hi ");
+//				System.out.println(itr.next());
+//			}
+//			System.out.println("AFter Teh While LOop ");
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//			System.out.println("InValid Or Empty List ");
+//		}
+//		
+//		System.out.println(list.size());
+		
 	}
 
 }
